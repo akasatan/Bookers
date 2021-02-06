191c8d0af6d9375d441bc21311ce62748cbe1b34
akasatan <aka435304@gmail.com>
2021-02-06 16:03:24 +0000

    [add]成功メッセージ、編集機能、詳細機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -14,16 +14,23 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book),notice: "Book was successfully created."
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book),notice: "Book was successfully updated."
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path,notice: "Book was successfully destroyed."
   end
 
   private
