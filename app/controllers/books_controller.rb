592517869024889e39050ec7f8f218b622e224fb
akasatan <aka435304@gmail.com>
2021-02-07 07:36:33 +0000

    [add]メンターさんに聞く前の段階

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book),notice: "Book was successfully created."
+    if book.save
+       redirect_to books_path,notice: "Book was successfully created."
+    else
+      render :index
+    end
   end
 
   def edit
@@ -22,9 +21,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book),notice: "Book was successfully updated."
+     book = Book.find(params[:id])
+     book.update(book_params)
+     redirect_to book_path(book),notice: 'Book was successfully updated.'
   end
 
   def destroy
@@ -37,5 +36,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
